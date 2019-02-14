c96863b6ab4c3e891c58a0f93c84dded1c82ffc7
Jane Lawson <git@jane.fyi>
2019-02-14 16:03:08 -0600

    Update functions.php
    + email cloak

--- functions.php
+++ functions.php
@@ -1,4 +1,16 @@
 <?php
+// Cloak emails
+	function email_cloaking_shortcode( $atts ) {
+		$atts = shortcode_atts(
+			array(
+				'email' => '',
+			),
+			$atts,
+			'cloak'
+		);
+		return antispambot( $atts['email'] );
+	}
+	add_shortcode( 'cloak', 'email_cloaking_shortcode' );
 // Registers menus
 	function register_my_menu() {
 		register_nav_menu(
